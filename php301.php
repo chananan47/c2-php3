eb41012a66a58fe724d21a4a38e853ccd420c758
chananan47 <namayura47@gmail.com>
2021-11-19 10:37:17 +0700

    Update v1

--- php301.php
+++ php301.php
@@ -1,2 +1,11 @@
 <?php
-echo "Welcome to My site";
+    $x = 10;
+    $y = 5;
+    echo "<h1>PHP Arithmetic Operators</h1>";
+    echo "<h1>x = $x and y = $y" , "<hr color ='red' siz = '4' /></h1>";
+    echo "x + y =" , $x + $y , "<br />";
+    echo "x - y =" , $x - $y , "<br />";
+    echo "x * y =" , $x * $y, "<br />";
+    echo "x / y =" , $x / $y, "<br />";
+    echo "x % y =" , $x % $y, "<br />";
+    echo "x ^ y =" , $x ** $y, "<br />";
--- php302.php
+++ php302.php
@@ -1,2 +1,11 @@
 <?php
-echo "Operator";
+    $x = 20;
+    echo "<h1>PHP Arithmetic Operators</h1>";
+    echo "<h1> x = $x <br /></h1>";
+    echo "<hr color = 'red' size='4px' />";
+
+    echo " x += : ", $x += 10 , "<br />"; //$x = $x + 10
+    echo " x -= : ", $x -= 10 , "<br />"; //$x = $x - 10
+    echo " x *= : ", $x *= 10 , "<br />"; //$x = $x * 10
+    echo " x /= : ", $x /= 10 , "<br />"; //$x = $x / 10
+    echo " x %= : ", $x %= 10 , "<br />"; //$x = $x % 10
